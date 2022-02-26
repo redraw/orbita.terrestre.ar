2d9c03e07b42c85d5e876d5749502cd071382499
redraw <agustinbv@gmail.com>
2022-02-26 15:59:29 -0300

    proxy x-vercel-ip headers

--- api/celestrak.go
+++ api/celestrak.go
@@ -3,8 +3,20 @@ package handler
 import (
 	"fmt"
 	"io"
+	"strings"
 	"net/http"
 )
+
+
+func copyGeoHeaders(src, dest http.Header) {
+	for key, values := range src {
+		if strings.HasPrefix(strings.ToLower(key), "x-vercel-ip") {
+			for _, value := range values {
+				dest.Add(key, value)
+			}
+		}
+	}
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("https://celestrak.com/NORAD/elements/gp.php?%s", r.URL.RawQuery)
@@ -16,5 +28,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer response.Body.Close()
 
+	copyGeoHeaders(r.Header, w.Header())
+	w.WriteHeader(response.StatusCode)
 	io.Copy(w, response.Body)
 }
