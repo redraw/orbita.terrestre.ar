697fd71eb30806e8dcd4f154155399af566001da
Agustin Bacigalup <agustinbv@gmail.com>
2024-05-07 07:28:40 -0300

    Update index.go

--- api/geoip/index.go
+++ api/geoip/index.go
@@ -8,7 +8,7 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	header := w.Header()
 	for key, values := range r.Header {
-		if strings.HasPrefix(strings.ToLower(key), "x-vercel-ip") {
+		if strings.HasPrefix(strings.ToLower(key), "cf-ip") {
 			for _, value := range values {
 				header.Add(key, value)
 			}
